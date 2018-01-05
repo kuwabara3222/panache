504a273c8803407ce8c86b1cd66b955d17a033f9
DESKTOP-IJ6DPIG\06 <kuwabara3222@gmail.com>
2018-01-05 17:24:27 +0900

    0105

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
-$(function(){
-    
+$(function () {
+
+
+
+  $('body').vegas({
+    slides: [
+      {
+        src: '../img/mv1@2x.jpg'
+      },
+      {
+        src: '../img/mv2@2x.jpg'
+      },
+      {
+        src: '../img/mv3@2x.jpg'
+      }
+        ]
+  });
+
 })
